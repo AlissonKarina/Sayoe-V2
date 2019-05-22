f342720c33048577bb06a3f214c71ee3ee1aba1f
AlissonArroyo <alisson.arroyo@unmsm.edu.pe>
2019-05-22 15:35:34 -0500

    idusuario -> idunayoperfil

--- routes/web.php
+++ routes/web.php
@@ -10,7 +10,7 @@
 | and give it the Closure to call when that URI is requested.
 |
 */
-$router->get('/{id}', 'UnayoePerfilController@getUsuario');
+$router->get('/{idUsuario}', 'UnayoePerfilController@getUsuario');
 $router->get('/facultades', 'FacultadController@index');
 $router->group(['prefix'=>'api/v1'], function() use($router){
  
