479dd54158be2083c535f5b546e2c0d24a8c4ec4
AlissonArroyo <alisson.arroyo@unmsm.edu.pe>
2019-06-05 15:30:20 -0500

    helper

--- app/Http/Helper/Helper.php
+++ app/Http/Helper/Helper.php
@@ -16,7 +16,7 @@ class Helper
         return $nombre;
     }
     
-    public function fecha($fecha){
+    public static function fecha($fecha){
         $dia = $fecha[0].$fecha[1];
         $mes = $fecha[3].$fecha[4];
         $anho = $fecha[6].$fecha[7].$fecha[8].$fecha[9];
@@ -28,7 +28,7 @@ class Helper
         ];
     }
 
-    public function semestre($mes, $anho){
+    public static function semestre($mes, $anho){
         $semestre='';
         if($mes=='01' or $mes=='02' or $mes=='03'){
             $semestre = $anho.'-0';
