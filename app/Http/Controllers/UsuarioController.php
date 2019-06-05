3a4d8e763732ad41702ecc00bd38203775ef0fb4
AlissonArroyo <alisson.arroyo@unmsm.edu.pe>
2019-06-05 14:59:01 -0500

    fecha

--- app/Http/Controllers/UsuarioController.php
+++ app/Http/Controllers/UsuarioController.php
@@ -18,5 +18,13 @@ class UsuarioController extends Controller
         
     }
 
+    public function fecha($fecha){
+        $dia = $fecha[0].$fecha[1];
+        $mes = $fecha[3].$fecha[4];
+        $anho = $fecha[6].$fecha[7].$fecha[8].$fecha[9];
+
+        return $dia;
+    }
+
     //
 }
