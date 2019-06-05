f4a828918e7ee48dd36bdfc5774b10cf234130fb
AlissonArroyo <alisson.arroyo@unmsm.edu.pe>
2019-06-05 12:16:03 -0500

    not found

--- app/Http/Controllers/AlumnoController.php
+++ app/Http/Controllers/AlumnoController.php
@@ -32,7 +32,7 @@ class AlumnoController extends Controller
     public static function getUsuario($idUsuario){
         /* return $model; */
         $alumno = Alumno::where('id_usuario', '=' , $idUsuario)->get();
-        
+        return $alumno[0];
         if($alumno == [])
             return 0;
         return $alumno[0]['codigo'];
