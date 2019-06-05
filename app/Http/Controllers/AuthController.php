b9f7f623f6765e98e31d746479c20372bc50bf11
AlissonArroyo <alisson.arroyo@unmsm.edu.pe>
2019-06-05 11:36:24 -0500

    not found

--- app/Http/Controllers/AuthController.php
+++ app/Http/Controllers/AuthController.php
@@ -21,7 +21,7 @@ class AuthController extends BaseController
     
     protected function jwt(Usuario $user) {
         try{
-            $id = UsuarioController::getUsuario($user->id, App\UnayoePerfil::class);
+            $id = UsuarioController::getUsuario($user->id, UnayoePerfil::class);
         }catch(Exception $e){
             return $e->getMessage();
             /* $id = UsuarioController::getUsuario($user->id, 'Alumno'); */
