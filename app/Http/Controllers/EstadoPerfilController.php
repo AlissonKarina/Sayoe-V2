390d10f25147a1ffe08decdb00a803d664501cf3
AlissonArroyo <alisson.arroyo@unmsm.edu.pe>
2019-06-06 03:09:22 -0500

    names helper

--- app/Http/Controllers/EstadoPerfilController.php
+++ app/Http/Controllers/EstadoPerfilController.php
@@ -18,7 +18,7 @@ class EstadoPerfilController extends Controller
                               ->where('anho','=', $request->anho)
                               ->where('semestre','=', $semestre)->get();
  */
-        $perfiles = PerfilPsicologico::with('preguntas')
+        $perfiles = PerfilPsicologico::with('evaluaciones')
                 ->where('codigo_alumno','=', $request->codigo)
                 ->where('anho','=', $request->anho)
                 ->where('semestre','=', $semestre)->get();
