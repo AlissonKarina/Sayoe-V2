dd9e8831f232ab96a6c9291149e2b7e331c0d9cb
AlissonArroyo <alisson.arroyo@unmsm.edu.pe>
2019-06-07 00:35:46 -0500

    listo, continuar

--- app/Http/Resources/PerfilPsicologicoEstadoResource.php
+++ app/Http/Resources/PerfilPsicologicoEstadoResource.php
@@ -13,7 +13,6 @@ class PerfilPsicologicoEstadoResource extends JsonResource
             'semestre' => $this->semestre,
             'fecha_vencimiento' => $this->fecha_limite,
             'estadosPerfil' => EstadoPerfilResource::collection($this->whenLoaded('estadosPerfil')),
-            'estadosPerfilc2' => $this->estadosPerfil->descripcion
         ];
     }
 }
