3356776aa954ab2e6710c4aca01c709dea66e643
AlissonArroyo <alisson.arroyo@unmsm.edu.pe>
2019-06-07 00:24:06 -0500

    prueba02 EstadoPerfilResource titulo_secundario

--- app/Http/Resources/EstadoPerfilResource.php
+++ app/Http/Resources/EstadoPerfilResource.php
@@ -9,8 +9,8 @@ class EstadoPerfilResource extends JsonResource
     public function toArray($request)
     {
         return [
-            'evaluacion' => $this->cuestionarioEvaluacion,
-            'cuestionarioEvaluacion' => CuestionarioEvaluacionShortResource::collection($this->whenLoaded('cuestionarioEvaluacion')),
+            'evaluacion' => $this->cuestionarioEvaluacion->titulo_secundario,
+            
         ];
     }
 }
