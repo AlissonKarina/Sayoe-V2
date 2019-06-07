4740e79266478b1a6e96156b0ef2b833ceaf1c10
AlissonArroyo <alisson.arroyo@unmsm.edu.pe>
2019-06-07 15:30:23 -0500

    estado - pruena 03

--- app/Http/Controllers/EstadoPerfilController.php
+++ app/Http/Controllers/EstadoPerfilController.php
@@ -16,23 +16,24 @@ class EstadoPerfilController extends Controller
     public function evaluacionesPendientes(Request $request)
     {
       $semestre = Helper::semestre($request->mes);
-     /*  $perfiles = PerfilPsicologico::with('estadosPerfil')
+      $perfiles = PerfilPsicologico::with('estadosPerfil')
                 ->join('estado_perfiles', 'estado_perfiles.id_perfil_psico', '=', 'perfiles_psicologicos.id')
-                ->where('codigo_alumno','=', $request->codigo)
-                ->where('anho','=', $request->anho)
-                ->where('semestre','=', $semestre)->get(); */
+                ->where('perfiles_psicologicos.codigo_alumno','=', $request->codigo)
+                ->where('perfiles_psicologicos.anho','=', $request->anho)
+                ->where('perfiles_psicologicos.semestre','=', $semestre)
+                ->where('estado_perfiles.estado','=', '0')->get();
                 
-      $perfiles = DB::table('perfiles_psicologicos')
+      /* $perfiles = DB::table('perfiles_psicologicos')
             ->join('estado_perfiles', 'estado_perfiles.id_perfil_psico', '=', 'perfiles_psicologicos.id')
             ->where('perfiles_psicologicos.codigo_alumno','=', $request->codigo)
             ->where('perfiles_psicologicos.anho','=', $request->anho)
             ->where('perfiles_psicologicos.semestre','=', $semestre)
             ->where('estado_perfiles.estado','=', '0')
-            ->get();
+            ->get(['id', 'anho' 'semestre', 'fecha_limite']); */
             /* ->select('users.*', 'contacts.phone', 'orders.price')
             ->get(); */
-      /* return PerfilPsicologicoEstadoResource::collection($perfiles); */
-      return $perfiles;
+      return PerfilPsicologicoEstadoResource::collection($perfiles);
+     /*  return $perfiles; */
     }
 
     public function evaluacionesRealizadas(Request $request)
