d77a6113f17c536abae95c85d7d9c06c2f69c461
AlissonArroyo <alisson.arroyo@unmsm.edu.pe>
2019-06-08 02:16:11 -0500

    id_perfil---

--- app/Http/Resources/PerfilPsicologicoEstadoResource.php
+++ app/Http/Resources/PerfilPsicologicoEstadoResource.php
@@ -9,7 +9,7 @@ class PerfilPsicologicoEstadoResource extends JsonResource
     public function toArray($request)
     {
         return [
-            'id_perfil_psico' => $this->id,
+            'id_perfil_psico' => $this->id_perfil,
             'anho' => $this->anho,
             'semestre' => $this->semestre,
             'fecha_vencimiento' => $this->fecha_limite,
