a1d99a5600e02f6f739e834dcf9c5e7ef000c2c7
AlissonArroyo <alisson.arroyo@unmsm.edu.pe>
2019-06-08 04:38:53 -0500

    evalauciones -- prueba

--- app/Http/Controllers/EstadoPerfilController.php
+++ app/Http/Controllers/EstadoPerfilController.php
@@ -38,13 +38,30 @@ class EstadoPerfilController extends Controller
             ->where('semestre','=', $semestre)
             ->get();
                         */
-      $total = DB::table('perfiles_psicologicos')
+      $perfiles = DB::table('perfiles_psicologicos')
             ->join('estado_perfiles', 'perfiles_psicologicos.id_perfil', '=', 'estado_perfiles.id_perfil_psico')
+            ->join('cuestionario_evaluaciones', 'cuestionario_evaluaciones.id','=','estado_perfiles.id_cuest_eval')
             ->where('perfiles_psicologicos.codigo_alumno','=', $request->codigo)
             ->where('perfiles_psicologicos.anho','=', $request->anho)
             ->where('perfiles_psicologicos.semestre','=', $semestre)
             ->where('estado_perfiles.estado','=',$estado)
+            ->select('perfiles_psicologicos.id_perfil','perfiles_psicologicos.fecha_limite',
+              'perfiles_psicologicos.semestre', 'perfiles_psicologicos.anho',
+              'estado_perfiles.id', 'cuestionario_evaluaciones.titulo_secundario',
+              'cuestionario_evaluaciones.nro_preguntas')
             ->get();
+            
+            /* foreach ($perfiles as $perfil){
+              $array = [
+                "id_perfil_psico" => $perfil->id_perfil,
+                "anho" => $perfil->anho,
+                "semestre" => $perfil->semestre,
+                "fecha_vencimiento" => $perfil->fecha_limite,
+                "evaluaciones" => EstadoPerfilResource::collection($evaluaciones),
+              ];
+              array_push($arrayTotal['data'],$array);
+          } */ 
+
     /*   foreach ($perfiles as $perfil){
           $evaluaciones = EstadoPerfil::where('id_perfil_psico','=',$perfil->id_perfil)
             ->where('estado','=',$estado)
@@ -60,7 +77,7 @@ class EstadoPerfilController extends Controller
       } */
 
       /* return response()->json($arrayTotal); */
-      return response()->json($total);
+      return response()->json($perfiles);
     }
 
     public function obtenerResultado(Request $request)
