518b4be0bdbd0ab27f64d12e5f61adb6b5066286
AlissonArroyo <alisson.arroyo@unmsm.edu.pe>
2019-06-08 03:22:37 -0500

    inteeeeeentooo

--- app/Http/Resources/EstadoPerfilResource.php
+++ app/Http/Resources/EstadoPerfilResource.php
@@ -8,17 +8,14 @@ class EstadoPerfilResource extends JsonResource
 {
     public function toArray($request)
     {
-        return [
-            /* 'data '=> $this->when($this->estado == 0, function () {
-                return [ */
-                    'id_estado_perfil' => $this->id,
-                    'id_cuest_eval' => $this->cuestionarioEvaluacion->id,
-                    'titulo_secundario' => $this->cuestionarioEvaluacion->titulo_secundario,
-                    'estado' => $this->estado,
-                    'nro_preguntas' => $this->cuestionarioEvaluacion->nro_preguntas,
-            /*     ];
-            }) */
-            
-        ];
+        $this->when($this->estado == 0, function () {
+            return [
+                'id_estado_perfil' => $this->id,
+                'id_cuest_eval' => $this->cuestionarioEvaluacion->id,
+                'titulo_secundario' => $this->cuestionarioEvaluacion->titulo_secundario,
+                'estado' => $this->estado,
+                'nro_preguntas' => $this->cuestionarioEvaluacion->nro_preguntas,
+            ];
+        });
     }
 }
