4dd1a85bbf4f98798937592f772499333bffb6e6
AULA F.DE ING.DE SISTEMAS E INF <AULAFISI@UNMSM.pe>
2019-06-13 20:10:59 -0500

    cmabio , todo a un mismo nivel

--- app/Http/Resources/PerfilPsicologicoResource.php
+++ app/Http/Resources/PerfilPsicologicoResource.php
@@ -10,7 +10,11 @@ class PerfilPsicologicoResource extends JsonResource
     {
         return [
             'fecha_resuelto' => $this->fecha_resuelto,
-            'alumno' => new AlumnoShortResource($this->whenLoaded('alumno')),   
+            'nombre' => $this->alumno->persona->apellido_paterno." ".$this->alumno->persona->apellido_materno.", ".$this->alumno->persona->nombre,
+            'codigo' => $this->alumno->codigo,
+            'escuela' => $this->alumno->escuelaProfesional->nombre,
+            'situacion' => $this->alumno->situacion
+            /* 'alumno' => new AlumnoShortResource($this->whenLoaded('alumno')),  */  
         ];
     }
 }
