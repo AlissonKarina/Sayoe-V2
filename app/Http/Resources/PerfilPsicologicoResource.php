54025d24b1b596c8e246b0d828d0416d307b4382
AULA F.DE ING.DE SISTEMAS E INF <AULAFISI@UNMSM.pe>
2019-06-13 21:20:58 -0500

    cmabio , todo a un mismo nive

--- app/Http/Resources/PerfilPsicologicoResource.php
+++ app/Http/Resources/PerfilPsicologicoResource.php
@@ -9,6 +9,7 @@ class PerfilPsicologicoResource extends JsonResource
     public function toArray($request)
     {
         return [
+            'id_perfil_psico' => $this->id_perfil,
             'fecha_resuelto' => $this->fecha_resuelto,
             'nombre' => $this->alumno->persona->apellido_paterno." ".$this->alumno->persona->apellido_materno.", ".$this->alumno->persona->nombre,
             'codigo' => $this->alumno->codigo,
