b664bb13d589c9be5d6a4c8e67b83be0647b700a
rubissm <rubi.santivanez@unmsm.edu.pe>
2019-06-16 02:04:36 -0500

    Datos de usuarios

--- database/seeds/AlumnoSeeder.php
+++ database/seeds/AlumnoSeeder.php
@@ -12,26 +12,219 @@ class AlumnoSeeder extends Seeder
      */
     public function run()
     {
+        Alumno::create([
+            'codigo' => '07200126',
+            'ciclo' => 'X',
+            'situacion' => "R",
+            'promedio' => '12.4',
+            'anho_ingreso' => '2007',
+            'estado_permanencia' => 'Activo',
+            'total_creditos' => '115',
+            'dni' => '43695481',
+            'id_usuario' => '1000000',
+            'id_escuela' => '2'
+        ]);
+
+        Alumno::create([
+            'codigo' => '09200053',
+            'ciclo' => 'IX',
+            'situacion' => "O",
+            'promedio' => '12.6',
+            'anho_ingreso' => '2009',
+            'estado_permanencia' => 'Activo',
+            'total_creditos' => '110',
+            'dni' => '44603086',
+            'id_usuario' => '1000001',
+            'id_escuela' => '2'
+        ]);
+
+        Alumno::create([
+            'codigo' => '09200054',
+            'ciclo' => 'X',
+            'situacion' => "R",
+            'promedio' => '11.2',
+            'anho_ingreso' => '2009',
+            'estado_permanencia' => 'Activo',
+            'total_creditos' => '92',
+            'dni' => '70043237',
+            'id_usuario' => '1000002',
+            'id_escuela' => '2'
+        ]);
+
+        Alumno::create([
+            'codigo' => '09200139',
+            'ciclo' => 'IX',
+            'situacion' => "R",
+            'promedio' => '13.2',
+            'anho_ingreso' => '2009',
+            'estado_permanencia' => 'Activo',
+            'total_creditos' => '110',
+            'dni' => '45912324',
+            'id_usuario' => '1000003',
+            'id_escuela' => '2'
+        ]);
+
+        Alumno::create([
+            'codigo' => '09200154',
+            'ciclo' => 'VIII',
+            'situacion' => "O",
+            'promedio' => '11.2',
+            'anho_ingreso' => '2009',
+            'estado_permanencia' => 'Activo',
+            'total_creditos' => '92',
+            'dni' => '46481023',
+            'id_usuario' => '1000004',
+            'id_escuela' => '2'
+        ]);
+
+        Alumno::create([
+            'codigo' => '09200159',
+            'ciclo' => 'VIII',
+            'situacion' => "O",
+            'promedio' => '13.6',
+            'anho_ingreso' => '2009',
+            'estado_permanencia' => 'Activo',
+            'total_creditos' => '98',
+            'dni' => '46519414',
+            'id_usuario' => '1000005',
+            'id_escuela' => '2'
+        ]);
+
+        Alumno::create([
+            'codigo' => '09200229',
+            'ciclo' => 'VIII',
+            'situacion' => "O",
+            'promedio' => '13.8',
+            'anho_ingreso' => '2009',
+            'estado_permanencia' => 'No activo',
+            'total_creditos' => '98',
+            'dni' => '70078950',
+            'id_usuario' => '1000006',
+            'id_escuela' => '2'
+        ]);
+
+        Alumno::create([
+            'codigo' => '10200064',
+            'ciclo' => 'VIII',
+            'situacion' => "O",
+            'promedio' => '13.8',
+            'anho_ingreso' => '2010',
+            'estado_permanencia' => 'Activo',
+            'total_creditos' => '98',
+            'dni' => '45139174',
+            'id_usuario' => '1000007',
+            'id_escuela' => '2'
+        ]);
+
+        Alumno::create([
+            'codigo' => '10200068',
+            'ciclo' => 'IX',
+            'situacion' => "R",
+            'promedio' => '13.1',
+            'anho_ingreso' => '2010',
+            'estado_permanencia' => 'Activo',
+            'total_creditos' => '97',
+            'dni' => '46887361',
+            'id_usuario' => '1000008',
+            'id_escuela' => '2'
+        ]);
+
+        Alumno::create([
+            'codigo' => '10200071',
+            'ciclo' => 'VIII',
+            'situacion' => "R",
+            'promedio' => '13.1',
+            'anho_ingreso' => '2010',
+            'estado_permanencia' => 'Activo',
+            'total_creditos' => '85',
+            'dni' => '47465301',
+            'id_usuario' => '1000009',
+            'id_escuela' => '2'
+        ]);
+
+        Alumno::create([
+            'codigo' => '10200143',
+            'ciclo' => 'VIII',
+            'situacion' => "R",
+            'promedio' => '13.1',
+            'anho_ingreso' => '2010',
+            'estado_permanencia' => 'Activo',
+            'total_creditos' => '72',
+            'dni' => '43386175',
+            'id_usuario' => '1000010',
+            'id_escuela' => '2'
+        ]);
+
+        Alumno::create([
+            'codigo' => '10200144',
+            'ciclo' => 'VIII',
+            'situacion' => "R",
+            'promedio' => '13.1',
+            'anho_ingreso' => '2010',
+            'estado_permanencia' => 'Activo',
+            'total_creditos' => '79',
+            'dni' => '43061789',
+            'id_usuario' => '1000011',
+            'id_escuela' => '2'
+        ]);
+
+        Alumno::create([
+            'codigo' => '10200151',
+            'ciclo' => 'VIII',
+            'situacion' => "R",
+            'promedio' => '13.1',
+            'anho_ingreso' => '2010',
+            'estado_permanencia' => 'Activo',
+            'total_creditos' => '76',
+            'dni' => '47676481',
+            'id_usuario' => '1000012',
+            'id_escuela' => '2'
+        ]);
+
+        Alumno::create([
+            'codigo' => '10200156',
+            'ciclo' => 'VIII',
+            'situacion' => "R",
+            'promedio' => '13.9',
+            'anho_ingreso' => '2010',
+            'estado_permanencia' => 'No activo',
+            'total_creditos' => '73',
+            'dni' => '46837299',
+            'id_usuario' => '1000013',
+            'id_escuela' => '2'
+        ]);
+
+        Alumno::create([
+            'codigo' => '10200157',
+            'ciclo' => 'VIII',
+            'situacion' => "O",
+            'promedio' => '10.1',
+            'anho_ingreso' => '2010',
+            'estado_permanencia' => 'Activo',
+            'total_creditos' => '76',
+            'dni' => '47573179',
+            'id_usuario' => '1000014',
+            'id_escuela' => '2'
+        ]);
+
+        Alumno::create([
+            'codigo' => '10200166',
+            'ciclo' => 'VII',
+            'situacion' => "O",
+            'promedio' => '13.1',
+            'anho_ingreso' => '2010',
+            'estado_permanencia' => 'Activo',
+            'total_creditos' => '62',
+            'dni' => '46035060',
+            'id_usuario' => '1000015',
+            'id_escuela' => '2'
+        ]);
+
         for($i = 2 ; $i <= 11 ; $i ++){
             Alumno::create([
                 'codigo' => '162000'.$i,
                 'ciclo' => $i,
                 'situacion' => "O",
-                'promedio' => '15.6',
-                'anho_ingreso' => '2016',
-                'estado_permanencia' => 'Activo',
-                'total_creditos' => '82',
-                'dni' => '987654'.$i,
-                'id_usuario' => $i,
-                'id_escuela' => '1'
-            ]);
-        }
-
-        for($i = 12 ; $i <= 21 ; $i ++){
-            Alumno::create([
-                'codigo' => '162000'.$i,
-                'ciclo' => '1',
-                'situacion' => "R",
                 'promedio' => '15.6',
                 'anho_ingreso' => '2016',
                 'estado_permanencia' => 'Activo',
--- database/seeds/PersonaSeeder.php
+++ database/seeds/PersonaSeeder.php
@@ -13,17 +13,303 @@ class PersonaSeeder extends Seeder
     public function run()
     {
         Persona::create([
-                'dni' => '9876542',
-                'nombre' => "Luis David ",
-                'apellido_paterno' => "Yauri",
-                'apellido_materno' => "Martinez",
-                'sexo' => '0',
-                'fecha_nacimiento' => "2001-10-05",
-                'foto' => 'https://autoescuela-aula4.es/wp-content/uploads/2016/01/chico-carnet-conducir.jpg',
-                'telefono' => "8888-888",
-                'celular' => "9999-9999",
-                'direccion' => "Miraflores ",
-                'correo_personal' => "david.yauri"."@gamil.com",
+            'dni' => '9876542',
+            'nombre' => "Roquer",
+            'apellido_paterno' => "Yupanqui",
+            'apellido_materno' => "Ccopa",
+            'sexo' => '0',
+            'fecha_nacimiento' => "1986-08-12",
+            'foto' => 'https://autoescuela-aula4.es/wp-content/uploads/2016/01/chico-carnet-conducir.jpg',
+            'telefono' => "8888-888",
+            'celular' => "92570-4675",
+            'direccion' => "Callao - Callao",
+            'correo_personal' => "roquer.yupanqui"."@gmail.com",
+
+        ]);
+
+        Persona::create([
+            'dni' => '9876542',
+            'nombre' => "Tania Victoria",
+            'apellido_paterno' => "Escobedo",
+            'apellido_materno' => "Cruzado",
+            'sexo' => '1',
+            'fecha_nacimiento' => "1987-08-21",
+            'foto' => 'https://autoescuela-aula4.es/wp-content/uploads/2016/01/chico-carnet-conducir.jpg',
+            'telefono' => "8888-888",
+            'celular' => "93537-6063",
+            'direccion' => "Puente Piedra - Lima",
+            'correo_personal' => "taviescobedo"."@gmail.com",
+
+        ]);
+
+        Persona::create([
+            'dni' => '70043237',
+            'nombre' => "Erik David",
+            'apellido_paterno' => "Guido",
+            'apellido_materno' => "Balarezo",
+            'sexo' => '0',
+            'fecha_nacimiento' => "1991-10-20",
+            'foto' => 'https://autoescuela-aula4.es/wp-content/uploads/2016/01/chico-carnet-conducir.jpg',
+            'telefono' => "8888-888",
+            'celular' => "98624-5245",
+            'direccion' => "Breña - Lima",
+            'correo_personal' => "erikd.guiba"."@gmail.com",
+
+        ]);
+
+        Persona::create([
+            'dni' => '45912324',
+            'nombre' => "Carlos Marcelo",
+            'apellido_paterno' => "Bonifaz",
+            'apellido_materno' => "Sota",
+            'sexo' => '0',
+            'fecha_nacimiento' => "1989-09-04",
+            'foto' => 'https://autoescuela-aula4.es/wp-content/uploads/2016/01/chico-carnet-conducir.jpg',
+            'telefono' => "8888-888",
+            'celular' => "93088-7268",
+            'direccion' => "San Martín de Porres - Lima",
+            'correo_personal' => "zero_wii"."hotmail.com",
+
+        ]);
+
+        Persona::create([
+            'dni' => '46481023',
+            'nombre' => "Julio Felipe",
+            'apellido_paterno' => "Romero",
+            'apellido_materno' => "Cárdenas",
+            'sexo' => '0',
+            'fecha_nacimiento' => "1990-07-22",
+            'foto' => 'https://autoescuela-aula4.es/wp-content/uploads/2016/01/chico-carnet-conducir.jpg',
+            'telefono' => "8888-888",
+            'celular' => "98417-7316",
+            'direccion' => "La Victoria - Lima",
+            'correo_personal' => "jromeroc90"."@gmail.com",
+        ]);
+
+        Persona::create([
+            'dni' => '46519414',
+            'nombre' => "Alberto",
+            'apellido_paterno' => "Tataje",
+            'apellido_materno' => "Miranda",
+            'sexo' => '0',
+            'fecha_nacimiento' => "1989-10-24",
+            'foto' => 'https://autoescuela-aula4.es/wp-content/uploads/2016/01/chico-carnet-conducir.jpg',
+            'telefono' => "8888-888",
+            'celular' => "99889-2013",
+            'direccion' => "Puente Piedra - Lima",
+            'correo_personal' => "betomiranda276"."@gmail.com",
+        ]);
+
+        Persona::create([
+            'dni' => '70078950',
+            'nombre' => "Arez Miguel",
+            'apellido_paterno' => "Rodriguez",
+            'apellido_materno' => "Pajita",
+            'sexo' => '0',
+            'fecha_nacimiento' => "1992-02-01",
+            'foto' => 'https://autoescuela-aula4.es/wp-content/uploads/2016/01/chico-carnet-conducir.jpg',
+            'telefono' => "8888-888",
+            'celular' => "92739-4773",
+            'direccion' => "San Juan de Lurigancho - Lima",
+            'correo_personal' => "aresrodriguezpajita"."@gmail.com",
+        ]);
+
+        Persona::create([
+            'dni' => '45139174',
+            'nombre' => "Ingrid Fiorella",
+            'apellido_paterno' => "Rodriguez",
+            'apellido_materno' => "Valdivia",
+            'sexo' => '1',
+            'fecha_nacimiento' => "1988-05-20",
+            'foto' => 'https://autoescuela-aula4.es/wp-content/uploads/2016/01/chico-carnet-conducir.jpg',
+            'telefono' => "8888-888",
+            'celular' => "99174-1436",
+            'direccion' => "Villa María - Lima",
+            'correo_personal' => "infirova"."@gmail.com",
+        ]);
+
+        Persona::create([
+            'dni' => '46887361',
+            'nombre' => "Richar",
+            'apellido_paterno' => "Quispe",
+            'apellido_materno' => "Curampa",
+            'sexo' => '0',
+            'fecha_nacimiento' => "1988-05-20",
+            'foto' => 'https://autoescuela-aula4.es/wp-content/uploads/2016/01/chico-carnet-conducir.jpg',
+            'telefono' => "8888-888",
+            'celular' => "93053-7833",
+            'direccion' => "Villa María del Triunfo - Lima",
+            'correo_personal' => "infirova"."@gmail.com",
+        ]);
+
+        Persona::create([
+            'dni' => '47465301',
+            'nombre' => "Alexander Martin",
+            'apellido_paterno' => "Ramos",
+            'apellido_materno' => "Tito",
+            'sexo' => '0',
+            'fecha_nacimiento' => "1991-11-30",
+            'foto' => 'https://autoescuela-aula4.es/wp-content/uploads/2016/01/chico-carnet-conducir.jpg',
+            'telefono' => "8888-888",
+            'celular' => "98625-2159",
+            'direccion' => "Lima - Lima",
+            'correo_personal' => "alex_mart30"."@hotmail.com",
+        ]);
+
+        Persona::create([
+            'dni' => '43386175',
+            'nombre' => "Juan Anyelo",
+            'apellido_paterno' => "Anchante",
+            'apellido_materno' => "Candia",
+            'sexo' => '0',
+            'fecha_nacimiento' => "1988-05-20",
+            'foto' => 'https://autoescuela-aula4.es/wp-content/uploads/2016/01/chico-carnet-conducir.jpg',
+            'telefono' => "8888-888",
+            'celular' => "99174-1436",
+            'direccion' => "San Martín de Porres - Lima",
+            'correo_personal' => "anyelius_64"."@hotmail.com",
+        ]);
+
+        Persona::create([
+            'dni' => '43061789',
+            'nombre' => "Larry Steve",
+            'apellido_paterno' => "Barreto",
+            'apellido_materno' => "Trujillo",
+            'sexo' => '0',
+            'fecha_nacimiento' => "1988-05-20",
+            'foto' => 'https://autoescuela-aula4.es/wp-content/uploads/2016/01/chico-carnet-conducir.jpg',
+            'telefono' => "8888-888",
+            'celular' => "93143-4840",
+            'direccion' => "Los Olivos - Lima",
+            'correo_personal' => "larrysbarretot"."@gmail.com",
+        ]);
+
+        Persona::create([
+            'dni' => '47676481',
+            'nombre' => "Kevin Martín",
+            'apellido_paterno' => "Castañeda",
+            'apellido_materno' => "Zorrilla",
+            'sexo' => '0',
+            'fecha_nacimiento' => "1988-05-20",
+            'foto' => 'https://autoescuela-aula4.es/wp-content/uploads/2016/01/chico-carnet-conducir.jpg',
+            'telefono' => "8888-888",
+            'celular' => "94914-9804",
+            'direccion' => "Santa Anita - Lima",
+            'correo_personal' => "castanedakevin114"."@gmail.com",
+        ]);
+
+        Persona::create([
+            'dni' => '46837299',
+            'nombre' => "Angel Alfonso",
+            'apellido_paterno' => "Gonzales",
+            'apellido_materno' => "Molina",
+            'sexo' => '0',
+            'fecha_nacimiento' => "1991-11-02",
+            'foto' => 'https://autoescuela-aula4.es/wp-content/uploads/2016/01/chico-carnet-conducir.jpg',
+            'telefono' => "8888-888",
+            'celular' => "97273-4536",
+            'direccion' => "Lima - Lima",
+            'correo_personal' => "captel_1302"."@hotmail.com",
+        ]);
+
+        Persona::create([
+            'dni' => '47573179',
+            'nombre' => "Alberto",
+            'apellido_paterno' => "Gutierrez",
+            'apellido_materno' => "Espinoza",
+            'sexo' => '0',
+            'fecha_nacimiento' => "1988-05-20",
+            'foto' => 'https://autoescuela-aula4.es/wp-content/uploads/2016/01/chico-carnet-conducir.jpg',
+            'telefono' => "8888-888",
+            'celular' => "99174-1436",
+            'direccion' => "Villa El Salvador - Lima",
+            'correo_personal' => "algutierrezes"."@gmail.com",
+        ]);
+
+        Persona::create([
+            'dni' => '46035060',
+            'nombre' => "Leonardo Fulgencio",
+            'apellido_paterno' => "Quispe",
+            'apellido_materno' => "Torres",
+            'sexo' => '0',
+            'fecha_nacimiento' => "1989-10-28",
+            'foto' => 'https://autoescuela-aula4.es/wp-content/uploads/2016/01/chico-carnet-conducir.jpg',
+            'telefono' => "8888-888",
+            'celular' => "99174-1436",
+            'direccion' => "San Miguel - Lima",
+            'correo_personal' => "leo.elimbah"."@gmail.com",
+        ]);
+
+
+
+        Persona::create([
+            'dni' => '46887361',
+            'nombre' => "Juan Anyelor",
+            'apellido_paterno' => "Anchante",
+            'apellido_materno' => "Candia",
+            'sexo' => '0',
+            'fecha_nacimiento' => "1988-05-20",
+            'foto' => 'https://autoescuela-aula4.es/wp-content/uploads/2016/01/chico-carnet-conducir.jpg',
+            'telefono' => "8888-888",
+            'celular' => "99174-1436",
+            'direccion' => "Villa El Salvador - Lima",
+            'correo_personal' => "infirova"."@gmail.com",
+        ]);
+
+        Persona::create([
+            'dni' => '46887361',
+            'nombre' => "Juan Anyelor",
+            'apellido_paterno' => "Anchante",
+            'apellido_materno' => "Candia",
+            'sexo' => '0',
+            'fecha_nacimiento' => "1988-05-20",
+            'foto' => 'https://autoescuela-aula4.es/wp-content/uploads/2016/01/chico-carnet-conducir.jpg',
+            'telefono' => "8888-888",
+            'celular' => "99174-1436",
+            'direccion' => "Villa El Salvador - Lima",
+            'correo_personal' => "infirova"."@gmail.com",
+        ]);
+
+        Persona::create([
+            'dni' => '46887361',
+            'nombre' => "Juan Anyelor",
+            'apellido_paterno' => "Anchante",
+            'apellido_materno' => "Candia",
+            'sexo' => '0',
+            'fecha_nacimiento' => "1988-05-20",
+            'foto' => 'https://autoescuela-aula4.es/wp-content/uploads/2016/01/chico-carnet-conducir.jpg',
+            'telefono' => "8888-888",
+            'celular' => "99174-1436",
+            'direccion' => "Villa El Salvador - Lima",
+            'correo_personal' => "infirova"."@gmail.com",
+        ]);
+
+        Persona::create([
+            'dni' => '46887361',
+            'nombre' => "Juan Anyelor",
+            'apellido_paterno' => "Anchante",
+            'apellido_materno' => "Candia",
+            'sexo' => '0',
+            'fecha_nacimiento' => "1988-05-20",
+            'foto' => 'https://autoescuela-aula4.es/wp-content/uploads/2016/01/chico-carnet-conducir.jpg',
+            'telefono' => "8888-888",
+            'celular' => "99174-1436",
+            'direccion' => "Villa El Salvador - Lima",
+            'correo_personal' => "infirova"."@gmail.com",
+        ]);
+
+        Persona::create([
+            'dni' => '9876542',
+            'nombre' => "Luis David ",
+            'apellido_paterno' => "Yauri",
+            'apellido_materno' => "Martinez",
+            'sexo' => '0',
+            'fecha_nacimiento' => "2001-10-05",
+            'foto' => 'https://autoescuela-aula4.es/wp-content/uploads/2016/01/chico-carnet-conducir.jpg',
+            'telefono' => "8888-888",
+            'celular' => "9999-9999",
+            'direccion' => "Miraflores ",
+            'correo_personal' => "david.yauri"."@gmail.com",
 
         ]);
         
